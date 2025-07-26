8e5cbc61221d18bc4b9d007c92b2d15bc8e4e2ba
小吴来哩 <wuchang@gdcattsoft.com>
2025-07-26 21:05:27 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './assets/main.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+// element-plus 图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
